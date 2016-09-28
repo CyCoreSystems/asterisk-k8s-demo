65487bf7ee8cfe2d5621ecda1edef721dd429453
Seán C McCord <ulexus@gmail.com>
2016-09-28 18:52:40 -0400

    App scaling

--- live-demo/app/app.go
+++ live-demo/app/app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"strconv"
 	"time"
 
 	"golang.org/x/net/context"
@@ -9,6 +10,7 @@ import (
 	"github.com/CyCoreSystems/ari"
 	"github.com/CyCoreSystems/ari/ext/audio"
 	"github.com/CyCoreSystems/ari/ext/prompt"
+	"github.com/CyCoreSystems/dispatchers/deployment"
 )
 
 // State is the structure for storing application execution data
@@ -18,6 +20,8 @@ type State struct {
 	h *ari.ChannelHandle
 
 	digit string
+
+	tries int
 }
 
 type stateFn func(*State) stateFn
@@ -39,6 +43,7 @@ func app(cl *ari.Client, h *ari.ChannelHandle) {
 	}()
 
 	h.Answer()
+	time.Sleep(time.Second)
 
 	// Create the state struct
 	s := &State{
@@ -56,28 +61,57 @@ func app(cl *ari.Client, h *ari.ChannelHandle) {
 }
 
 func menu(s *State) stateFn {
+	if s.tries > 3 {
+		return invalid
+	}
+	s.tries++
+
 	ret, err := prompt.Prompt(s.ctx, s.h, nil, "sound:hello", "sound:please-enter-your", "sound:number")
 	if err != nil {
-		return nil
-	}
-
-	if ret.Status != prompt.Complete {
+		log.Println("Failed to play prompt", err)
 		return invalid
 	}
 
-	s.digit = ret.Data
-	return reply
+	switch ret.Status {
+	case prompt.Complete:
+		s.digit = ret.Data
+		return reply
+	case prompt.Timeout:
+		return menu
+	default:
+		log.Println("Prompt status not complete", ret.Status)
+		return invalid
+	}
 }
 
 func reply(s *State) stateFn {
+	log.Println("Sending reply", s.digit)
 	q := audio.NewQueue()
 	q.Add("sound:you-entered")
 	q.Add("digits:" + s.digit)
 	_, _ = q.Play(s.ctx, s.h, nil)
+	return scale
+}
+
+func scale(s *State) stateFn {
+	ni, err := strconv.Atoi(s.digit)
+	if err != nil {
+		log.Println("Failed to parse digit as number", err)
+		return nil
+	}
+	n := int32(ni)
+
+	err = deployment.Scale(&n)
+	if err != nil {
+		log.Println("Failed to scale asterisk", err)
+		return nil
+	}
+	log.Println("Scaled asterisk to ", n)
 	return nil
 }
 
 func invalid(s *State) stateFn {
-	audio.Play(s.ctx, s.h, "an-error-has-occurred")
+	log.Println("Playing error message")
+	audio.Play(s.ctx, s.h, "sound:an-error-has-occurred")
 	return nil
 }
