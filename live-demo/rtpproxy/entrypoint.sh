8cb4453a62feed683a9dacb688ec067a9688b994
Seán C McCord <ulexus@gmail.com>
2018-06-26 11:41:00 -0400

    fix rtpproxy script

--- live-demo/rtpproxy/entrypoint.sh
+++ live-demo/rtpproxy/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-set -e
-
 : ${CLOUD=""} # One of aws, azure, do, gcp, or empty
+if [ "$CLOUD" != "" ]; then
+   PROVIDER="-provider ${CLOUD}"
+fi
 
-: ${PRIVATE_IPV4="(netdiscover -field privatev4 -provider ${CLOUD})"}
-: ${PUBLIC_IPV4="(netdiscover -field publicv4 -provider ${CLOUD})"}
+: ${PRIVATE_IPV4="$(netdiscover -field privatev4 ${PROVIDER})"}
+: ${PUBLIC_IPV4="$(netdiscover -field publicv4 ${PROVIDER})"}
 
 : ${RTPPROXY_ARGS:="-f -A ${PUBLIC_IPV4} -F -l ${PRIVATE_IPV4} -m 20000 -M 30000 -s udp:127.0.0.1:7722 -d INFO"}
 
