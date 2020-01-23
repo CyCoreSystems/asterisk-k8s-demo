2b1fc3fcbd0646ba363dcb983ca0da5fa6d4f590
Seán C McCord <ulexus@gmail.com>
2020-01-23 11:04:23 -0500

    Allow configurable asterisk check file
    Asterisk will now allow an override to `CHECK_FILE`, which will cause
    Asterisk to wait until that file exists before actually starting.  The
    file is referenced from the `/etc/asterisk/` root.

--- live-demo/asterisk/entrypoint.sh
+++ live-demo/asterisk/entrypoint.sh
@@ -3,16 +3,18 @@ set -e
 
 # Default values
 : ${ASTERISK_ARGS:="-fp"}
+: ${CHECK_FILE:="ari.d/k8s-asterisk-config.conf"}
 
 # If we were given arguments, run them instead
 if [ $# -gt 0 ]; then
    exec "$@"
 fi
 
-if [ ! -e /etc/asterisk/ari.d/k8s-asterisk-config.conf ]; then
-   echo "configuration not available"
+if [ ! -e "/etc/asterisk/$CHECK_FILE" ]; then
+   echo "configuration not yet available"
    exit 1
 fi
+
 
 # Run Asterisk
 exec /usr/sbin/asterisk ${ASTERISK_ARGS}
