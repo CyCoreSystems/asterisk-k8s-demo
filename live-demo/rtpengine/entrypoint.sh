baa98ef2aad67f7c62a5efe7111e9dd94bb60c9a
Seán C McCord <ulexus@gmail.com>
2021-05-04 09:57:35 -0700

    fix rtpengine interface parameter

--- live-demo/rtpengine/entrypoint.sh
+++ live-demo/rtpengine/entrypoint.sh
@@ -7,7 +7,7 @@ fi
 : ${PRIVATE_IPV4="$(netdiscover -field privatev4 ${PROVIDER})"}
 : ${PUBLIC_IPV4="$(netdiscover -field publicv4 ${PROVIDER})"}
 
-: ${RTPENGINE_ARGS:="-f --listen-udp=${PRIVATE_IPV4}!${PUBLIC_IPV4} --listen-ng=${PRIVATE_IPV4}:22222 --port-min 20000 --port-max 30000 --log-stderr"}
+: ${RTPENGINE_ARGS:="-f -i ${PRIVATE_IPV4}!${PUBLIC_IPV4} --listen-ng=${PRIVATE_IPV4}:22222 --port-min 20000 --port-max 30000 --log-stderr"}
 
 # If we were given arguments, run them instead
 if [ $# -gt 0 ]; then
